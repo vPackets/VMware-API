c1487d9ac4619b772f31cedd5529cfe6cab58ad6
Nicolas MICHEL <nic@vPackets-MBPro.local>
2019-02-04 15:57:54 -0600

    Change VM list

--- api_lab_start.py
+++ api_lab_start.py
@@ -2,8 +2,8 @@ from api_func import *
 
 token = get_token_authentication()
 
-windows_vm = ["vm-186", "vm-188", "vm-197"]
+lab_vm = ["vm-186", "vm-188", "vm-197", "vm-1432"]
 print("-" * 50, "Starting Windows Virtual Machine ", "-" * 50)
-for vm in windows_vm:
+for vm in lab_vm:
     post_response = post(api="/vcenter/vm/" + vm + "/power/start", token = token)
     time.sleep(1)
